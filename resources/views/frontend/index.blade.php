00bf74445fc6e96de5d56306bff59d95084c4f67
Daryl Sterling, Jr. <daryl@latouraineinc.com>
2025-03-08 07:54:32 +1000

    New index page messaging

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -14,7 +14,7 @@
                 Queensland State Ladder
             </h1>
             <p class="mb-1 text-nd font-normal text-gray-500 dark:text-gray-400 sm:px-16 sm:text-xl xl:px-48">
-            This project is a collaborative initiative between clubs to create an interactive ladder ranking system for table tennis players in Queensland, utilising the RatingsCentral system
+            This project is a collaborative initiative between clubs to create an interactive ladder ranking system for table tennis players in Queensland, utilising the RatingsCentral system. Feel free to select the different categories, or search for an athlete. 
             </p>
             
             
