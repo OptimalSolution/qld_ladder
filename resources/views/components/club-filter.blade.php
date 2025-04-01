a9ccd51e68d772acf294d875f91468c41c6112fd
Daryl Sterling, Jr. <daryl@latouraineinc.com>
2025-04-01 11:37:38 +1000

    Widened the club listing so that users don't have to scroll so far

--- resources/views/components/club-filter.blade.php
+++ resources/views/components/club-filter.blade.php
@@ -11,7 +11,7 @@
     </button>
 
     <!-- Dropdown menu -->
-    <div id="clubListing" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
+    <div id="clubListing" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-80 dark:bg-gray-700 dark:divide-gray-600">
         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200 text-left" aria-labelledby="dropdownClubButton">
             <li>
                 <a href="{{ route($preferred_route, ['age_group' => $age_group, 'club_id' => 'all', 'club_slug' => 'combined', 'gender_group' => $gender_group]) }}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All Clubs Combined ({{ $club_groups->count() }} Clubs)</a>
