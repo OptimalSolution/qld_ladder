651c3a92ed6c81d49b876ab07ed3e7d9f24ac755
Daryl Sterling, Jr. <daryl@latouraineinc.com>
2025-04-01 13:05:22 +1000

    Bug fix: Logo responds to light and dark mode, desktop and mobile

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -1,7 +1,10 @@
 <nav class="border-b-2 border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-900">
     <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
         <a class="flex items-center space-x-3 rtl:space-x-reverse" href="/">
-            <img class="h-9" src="{{ secure_asset("img/logo-with-text.jpg") }}" alt="{{ app_name() }} Logo" />
+            <img class="h-10 dark:hidden hidden md:block" src="{{ secure_asset('img/logo-with-text-light.jpg') }}" alt="{{ app_name() }} Logo" />
+            <img class="h-10 dark:hidden block md:hidden" src="{{ secure_asset('img/logo-with-text-mobile-light.jpg') }}" alt="{{ app_name() }} Logo" />
+            <img class="h-10 hidden dark:md:block" src="{{ secure_asset('img/logo-with-text-dark.jpg') }}" alt="{{ app_name() }} Logo" />
+            <img class="h-10 hidden dark:block dark:md:hidden" src="{{ secure_asset('img/logo-with-text-mobile-dark.jpg') }}" alt="{{ app_name() }} Logo" />
         </a>
         <div class="flex items-center justify-end space-x-1 md:order-2 md:space-x-0 rtl:space-x-reverse">
             <button
