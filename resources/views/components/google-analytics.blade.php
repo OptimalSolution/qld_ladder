9076e8a976113810e3b7a77ee0d66c111ce55c43
Daryl Sterling, Jr. <daryl@latouraineinc.com>
2025-04-11 09:05:11 +1000

    Changed GA back to a way where it can be set in the admin Settings

--- resources/views/components/google-analytics.blade.php
+++ resources/views/components/google-analytics.blade.php
@@ -1,4 +1,4 @@
-@if (false && setting("google_analytics") !== "")
+@if (setting("google_analytics") !== "")
     @php
         $google_analytics = setting("google_analytics");
     @endphp
@@ -15,14 +15,3 @@
         gtag('config', '{{ $google_analytics }}');
     </script>
 @endif
-
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-NHT4H42X87"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-NHT4H42X87');
-</script>
