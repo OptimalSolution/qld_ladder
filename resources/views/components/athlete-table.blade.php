8f63a9c5874bb66ab11379c9d632aa833fdae7ab
Daryl Sterling, Jr. <daryl@latouraineinc.com>
2025-05-06 21:25:29 +1000

    Added a search feature

--- resources/views/components/athlete-table.blade.php
+++ resources/views/components/athlete-table.blade.php
@@ -4,31 +4,39 @@
     'emptyMessage' => 'No athletes are currently eligible for the ladder'
 ])
 
-<div id="scrollInfo" class="md:hidden text-xl text-gray-500 dark:text-gray-300 p-2 bg-gray-50 dark:bg-gray-800 flex items-center justify-end scroll-info transition-opacity duration-300">
-    <span>Scroll for more info</span>
+<div id="scrollInfo" class="md:hidden text-lg text-gray-500 dark:text-gray-300 p-2 bg-gray-50 dark:bg-gray-800 flex items-center justify-end scroll-info transition-opacity duration-300">
+    <span>Scroll table for more info</span>
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-1 mt-[2px]" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="animation: scrollArrow 0.5s ease-in-out infinite;">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
     </svg>
 </div>
 
-<div class="max-w-md justify-start mb-2 ">   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-            </svg>
-        </div>
-        <input type="search" id="athlete-search" class="block w-3/4 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-200 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search athletes..." />
-    </div>
-</div>
 
-
-
-<div id="tableContainer" {{ $attributes->merge(['class' => 'relative overflow-x-auto shadow-lg rounded-lg border border-gray-200']) }}>
+<div id="" class="relative overflow-x-auto shadow-lg rounded-lg border border-gray-600" {{ $attributes->merge(['class' => '']) }}>
     <table class="w-full text-lg text-left rtl:text-right text-gray-800 dark:text-gray-200">
         <thead class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+            <tr class="dark:bg-gray-900">
+                <th scope="col" class="px-3 py-3" colspan="{{ count($columns) - 1 }}">
+                    <label for="default-search" class="mb-1 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 start-0 flex items-center ml-1 ps-2 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </div>
+                        <input type="search" id="athlete-search" class="block w-80 m-0 p-2 ps-10 text-sm text-gray-900 border rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search athletes..." />
+                    </div>                    
+                </th>
+                <th scope="col" class="px-2 py-3 float-end">
+                    <button type="button" disabled class="flex items-center justify-center text-gray-400 bg-gray-200 cursor-not-allowed font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-700 dark:text-gray-500">
+                        <svg class="w-4 h-4 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
+                        </svg>
+                        Track Athlete(s) (Coming Soon)
+                    </button>
+                </th>
+            </tr>
+            <tr class="dark:bg-gray-700">
                 @if(in_array('rung', $columns))
                 <th scope="col" class="px-2 text-center">
                     Rung
@@ -61,6 +69,7 @@
                 @endif
             </tr>
         </thead>
+        
         <tbody>
             @if(!isset($athletes) || $athletes->isEmpty())
                 <tr class="bg-white dark:bg-gray-800">
