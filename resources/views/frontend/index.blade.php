397feae1b5f47e5bf2bfdd0eea1a00b1cd29196a
Daryl Sterling, Jr. <daryl@latouraineinc.com>
2025-07-13 08:35:59 +1000

    Getting rid of old paths

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -17,7 +17,7 @@
             This project is a collaborative initiative between clubs to create an interactive ladder ranking system for table tennis players in Queensland, utilising the RatingsCentral system. Feel free to select the different categories, or search for an athlete below:
             </p>
             <div class="flex flex-col sm:flex-row items-center justify-center gap-4 mt-6">
-                <a href="{{ route('age-groups') }}" class="w-32 sm:w-auto">
+                <a href="{{ route('ladder-filter') }}" class="w-32 sm:w-auto">
                     <div class="flex flex-col items-center justify-center p-4 bg-blue-100 dark:bg-blue-900 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
                         <svg class="w-6 h-6 text-blue-600 dark:text-blue-300 mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
@@ -27,7 +27,7 @@
                     </div>
                 </a>
                 
-                <a href="{{ route('club-groups') }}" class="w-32 sm:w-auto">
+                <a href="{{ route('ladder-filter') }}" class="w-32 sm:w-auto">
                     <div class="flex flex-col items-center justify-center p-4 bg-green-100 dark:bg-green-900 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
                         <svg class="w-6 h-6 text-green-600 dark:text-green-300 mb-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
