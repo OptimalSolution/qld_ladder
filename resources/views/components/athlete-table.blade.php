06843c0c87e1a57a0dae43aa417493392083573c
Daryl Sterling, Jr. <daryl@latouraineinc.com>
2026-05-06 21:39:19 +1000

    Removed rating change indicators since there's something wrong

--- resources/views/components/athlete-table.blade.php
+++ resources/views/components/athlete-table.blade.php
@@ -121,7 +121,7 @@
                            data-tooltip-placement="top"
                         @endif
                            class="text-blue-500">
-                            {{ $athlete->rating }} {!! $athlete->eventInfo?->ratingSign !!}
+                            {{ $athlete->rating }} {{-- $athlete->eventInfo?->ratingSign --}}
                         </a>
                         <div id="rating-tooltip-{{ $athlete->id }}" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                             {!! $athlete->eventInfo->relative_point_change ?? '' !!} since the last event
